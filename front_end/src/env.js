b71194c4e6253c82e08e90aa30ef11c19af866b8
hydrogen602 <hydrogen31415@gmail.com>
2022-05-15 17:41:05 -0500

    explicit error for rows is undefined bug

---
+++ front_end/src/env.js
@@ -0,0 +1,14 @@
+
+/**
+ * Get the url of the backend, differentiating for production and dev
+ * REACT_APP_USE_PRODUCTION_API can be set to true to always use the heroku backend
+ * @returns {string} the url, without a slash
+ */
+export function getURL() {
+    if (process.env.NODE_ENV === "production" || process.env.REACT_APP_USE_PRODUCTION_API === "true") {
+        return "https://deepcuts-apps.herokuapp.com";
+    }
+    else {
+        return "http://localhost:3001"
+    }
+}
